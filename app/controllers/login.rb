9b5d7ff5946df9b654d954867846b74ba30082a9
Christiane Kammerl <christiane@kammerl.com>
2015-10-02 10:55:23 -0700

    adds regster/login/logout routes

---
+++ app/controllers/login.rb
@@ -0,0 +1,35 @@
+get "/register" do
+  @user = User.new
+  erb :register
+end
+
+post "/users" do
+  @user = User.new(username: params[:username])
+  @user.password = params[:password]
+  if @user.save
+    authenticate(@user.id)
+    redirect "/"
+  else
+    erb :register
+  end
+end
+
+get "/login" do
+  erb :login
+end
+
+post "/login" do
+  user = User.find_by(username: params[:username])
+
+  if user.password == params[:password]
+    authenticate(user.id)
+    redirect "/"
+  else
+    redirect "/login"
+  end
+end
+
+get "/logout" do
+  log_out
+  redirect "/"
+end

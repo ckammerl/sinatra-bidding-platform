291225d6d99fa2709e31d6669e10643bffcbe18d
Christiane Kammerl <christiane@kammerl.com>
2015-10-02 10:57:37 -0700

    removes pw validation in db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
+
   has_many :items
+
   validates :username, { presence: true, uniqueness: true }
-  validates :password_hash, length: { is: 7 }
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
 

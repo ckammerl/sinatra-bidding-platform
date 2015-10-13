3b38fb6674d46a97f1ee9d9728d00e33c0088132
Christiane Kammerl <christiane@kammerl.com>
2015-10-13 16:45:58 -0700

    - Adds css class .invisible and toggles (hide/show) login / register forms - Implements ajax call for form submission

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,5 @@
 get "/" do
+  @user = User.new
   erb :index
 end
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,44 @@
 $(document).ready(function() {
 
+  $('#nav').on('click', 'a', function(e) {
+    e.preventDefault();
+    var $clickedLink = $(this).attr('id');
+
+    if ( $clickedLink === 'register' ) {
+      $('form').attr('id', 'register-form');
+      $('form').attr('action', '/users');
+    } else if ( $clickedLink === 'login' ) {
+      $('form').attr('id', 'login-form');
+      $('form').attr('action', '/login');
+    } else if ( $clickedLink === 'logout' ) {
+      console.log('log out TBD')
+    }
+
+    $('#form-wrapper').toggleClass('invisible');
+  });
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $('#form-wrapper').toggleClass('invisible');
+
+    var routeUrl = $(this).attr('action');
+    var formData = $(this).serialize();
+
+    var requestHandler = $.ajax({
+      url: routeUrl,
+      method: 'post',
+      data: formData
+    });
+
+    requestHandler.done(function(response, status) {
+      console.log("TBD next");
+    });
+
+    requestHandler.fail(function(jqXHR, textStatus, errorThrown) {
+      console.log("TBD next");
+    });
+
+  })
+
+
 });
